35886549c053a415feb99fe6a155740774fc66f5
Ana Clara Custodio <dev.anaccustodio@gmail.com>
2024-04-02 20:15:39 -0300

    add final project

---
+++ src/main/java/br/edu/iftm/testescalculadora/TestesCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.testescalculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestesCalculadoraApplication.class, args);
+	}
+
+}
